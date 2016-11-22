1dbb5a38e6b0e56c41c230dd236769cc417cd55a
Thiago Augusto Teixeira <thiago.teixeira@matera.com>
2016-11-22 15:49:59 -0200

    Chaging to use log and correct environments

--- python/moveSubtitles.py
+++ python/moveSubtitles.py
@@ -1,11 +1,22 @@
 import os
 import shutil
+import logging
 
-sourcePath = os.environ['NZBPP_DIRECTORY']
-destinationPath = os.environ['NZBPP_FINALDIR']
+logger = logging.getLogger('bj-sonarr')
+hdlr = logging.FileHandler('/tmp/bj-sonarr-subtitle.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr) 
+logger.setLevel(logging.WARNING)
 
+sourcePath = os.getenv('Sonarr_EpisodeFile_Path', 'default')
+destinationPath = os.getenv('Sonarr_EpisodeFile_Path', 'default')
 
-if  os.environ['NZBPP_TOTALSTATUS'] == "SUCCESS" and os.environ['NZBPP_UNPACKSTATUS'] == 2:
-	for file in files:
-		    if file.endswith('.srt'):
-		        shutil.copy(os.path.join(sourcePath,file), os.path.join(destinationPath,file))
+logger.info('sourcePath: ' + sourcePath)
+logger.info('destinationPath: ' + destinationPath)
+try:
+	for file in os.listdir(sourcePath):
+    		if file.endswith('.srt'):
+	    		shutil.copy(os.path.join(sourcePath,file), os.path.join(destinationPath,file))
+except Exception as e:
+	logger.error(e)
