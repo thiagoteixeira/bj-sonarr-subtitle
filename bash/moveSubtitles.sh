00da065fe8d8eaf938b6471a23058993231c034b
Thiago Teixeira <thiagoteixeiraoficial@gmail.com>
2016-11-23 18:37:09 -0300

    Removing unnecessary files and adding subtitle script

--- bash/moveSubtitles.sh
+++
@@ -1,9 +0,0 @@
-LOG_FILE = "/tmp/bj-sonarr-subtitle.log"
-
-SOURCE_PATH = $sonarr_episodefile_sourcepath
-DESTINATION_PATH = $sonarr_episodefile_path
-
-echo "SOURCE_PATH: $SOURCE_PATH " >> $LOG_FILE
-echo "DESTINATION_PATH: $DESTINATION_PATH " >> $LOG_FILE
-
-find $SOURCE_PATH '*.srt' -exec cp -vuni '{}' $DESTINATION_PATH ";"
